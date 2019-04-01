b608f4f9503264691508d331de5ad43c14d07b09
Vlado Cavric <vladocavric@gmail.com>
2019-04-01 17:12:29 +0200

    small edits on slider

--- js/functions.js
+++ js/functions.js
@@ -4,7 +4,10 @@ $(document).ready(function() {
 
 function slickInit() {
     $('.slider').slick({
+        infinite: true,
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 1,
+        // autoplay: true,
+        // autoplaySpeed: 5000,
     })
 }
